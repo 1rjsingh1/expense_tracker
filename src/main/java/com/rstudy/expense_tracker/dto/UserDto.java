bd5c9e03f69fac202c3b3435434bb7541b0984fa
Rj <1rjsingh1@gmail.com>
2021-09-17 12:57:50 +0530

    Create UserDto.java

---
+++ src/main/java/com/rstudy/expense_tracker/dto/UserDto.java
@@ -0,0 +1,59 @@
+package com.rstudy.expense_tracker.dto;
+
+public class UserDto {
+
+    private String id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long phone;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Long getPhone() {
+        return phone;
+    }
+
+    public UserDto(String id, String firstName, String lastName, String email, Long phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+}
