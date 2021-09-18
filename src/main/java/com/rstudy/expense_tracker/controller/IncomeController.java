1789a440cc12ecb552158496160ee3f453d74194
Ritika Goyal <54425330+Ritika372@users.noreply.github.com>
2021-09-18 12:13:49 +0530

    Income model updated

--- src/main/java/com/rstudy/expense_tracker/controller/IncomeController.java
+++ src/main/java/com/rstudy/expense_tracker/controller/IncomeController.java
@@ -1,6 +1,4 @@
 package com.rstudy.expense_tracker.controller;
-
-import com.rstudy.expense_tracker.repository.IncomeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +6,4 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1/income")
 public class IncomeController {
-    @Autowired
-    IncomeRepository incomeRepository;
 }
