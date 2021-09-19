665f3a925c1809127da43ce341599e5c80986639
Ritika Goyal <54425330+Ritika372@users.noreply.github.com>
2021-09-19 22:25:06 +0530

    Income controller put and delete

--- src/main/java/com/rstudy/expense_tracker/dto/UserDto.java
+++ src/main/java/com/rstudy/expense_tracker/dto/UserDto.java
@@ -1,5 +1,10 @@
 package com.rstudy.expense_tracker.dto;
 
+import com.rstudy.expense_tracker.model.Expense;
+import com.rstudy.expense_tracker.model.Income;
+
+import java.util.Map;
+
 public class UserDto {
     public UserDto(){
 
@@ -10,6 +15,34 @@ public class UserDto {
     private String lastName;
     private String email;
     private Long phone;
+    private Map<String, Expense> expenseMap;
+    private Map<String, Income> incomeMap;
+
+    public UserDto(String id, String firstName, String lastName, String email, Long phone, Map<String, Expense> expenseMap, Map<String, Income> incomeMap) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.expenseMap = expenseMap;
+        this.incomeMap = incomeMap;
+    }
+
+    public Map<String, Expense> getExpenseMap() {
+        return expenseMap;
+    }
+
+    public void setExpenseMap(Map<String, Expense> expenseMap) {
+        this.expenseMap = expenseMap;
+    }
+
+    public Map<String, Income> getIncomeMap() {
+        return incomeMap;
+    }
+
+    public void setIncomeMap(Map<String, Income> incomeMap) {
+        this.incomeMap = incomeMap;
+    }
 
     public void setId(String id) {
         this.id = id;
@@ -49,13 +82,5 @@ public class UserDto {
 
     public Long getPhone() {
         return phone;
-    }
-
-    public UserDto(String id, String firstName, String lastName, String email, Long phone) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
     }
 }
